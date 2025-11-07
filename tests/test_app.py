ee9123da07edf4a4f6c3379da474081afe9bbb1f
Mara <mara.caldarini@gmail.com>
2025-11-07 16:05:48 +0100

    testing pull requests with failing tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
-    assert "<h3>Your Messages</h3>" in response.data.decode("utf-8")
+    assert "<h2>Your Messages</h2>" in response.data.decode("utf-8")
     assert "<form action=" in response.data.decode("utf-8")
